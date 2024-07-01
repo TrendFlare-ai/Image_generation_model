90c941d5a15309a14ea744a82d2899c180cd7bbc
Abhiram162105 <abhiyanampally@gmail.comgit>
2024-07-01 22:51:39 +0530

    0.0.0.0

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ def query(payload):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=int(os.getenv('PORT', 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 8000)))
